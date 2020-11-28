d1b3b5ff886a14ea8b9a63a9d2a3c7d286123e41
Arcreuss <arcreuss74@gmail.com>
2020-11-28 15:34:06 +0100

    [ADD] New command => achieve a task & detected if task doesn't exist

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ print("hello\n Wellcome on TodoApp !")
 
 option_input = ''
 while option_input != 'q':
-	print("(q=quit , l=list, n=new, c=complete=, r=reset)\n")
+	print("(q=quit , l=list, n=new, c=complete, r=reset)\n")
 	option_input = input('what do you want to do ? ').lower()
 
 	if option_input=='q':
@@ -19,7 +19,12 @@ while option_input != 'q':
 		tasks.append(input('write your new task : '))
 
 	elif option_input=='c':
-		pass
+		to_delete = input('which task do you want to delete ? ')
+
+		if to_delete in tasks:
+			tasks.remove(to_delete)
+		else:
+			print("task unknown, back to main menu.")
 
 	elif option_input=='r':
 		pass
