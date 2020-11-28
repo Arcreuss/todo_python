e52f6820cc7961233c140fea32d8fad1e34a9463
Arcreuss <arcreuss74@gmail.com>
2020-11-28 15:07:30 +0100

    [ADD] Infite loop quitting option

---
+++ app.py
@@ -0,0 +1,27 @@
+tasks = []
+
+print("hello\n Wellcome on TodoApp !")
+
+option_input = ''
+while option_input != 'q':
+	print("(q=quit , l=list, n=new, c=complete=, r=reset)\n")
+	option_input = input('what do you want to do ? ').lower()
+
+
+	if option_input=='q':
+		print("thanks, bye")
+	
+	elif option_input=='l':
+		pass #fait moi confiance je gère./
+
+	elif option_input=='n':
+		pass
+
+	elif option_input=='c':
+		pass
+
+	elif option_input=='r':
+		pass
+
+	else:
+		pass
